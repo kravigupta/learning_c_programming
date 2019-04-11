143e6391d63a040794312c83b7748171db2408b4
Ravi Kumar Gupta <kravi.gupta@gmail.com>
2019-04-11 20:45:24 +0530

    Hello World Commit :)

---
+++ hello.c
@@ -0,0 +1,15 @@
+// This is a comment
+
+// This is a preprocessor command to include the standard input output library.
+#include <stdio.h>
+
+// Entry point for a program. Always the function with name main
+int main() // function signature is <type> <func-name>()
+{
+  /** 
+   * Another type of comment, can be multiline.
+   */ 
+  // printing something here..
+  printf("Hello World!!\n"); // another type of comment after the statement. Single line
+  return 0; // returning something.. the type of returning value has to match the function signature
+}
